b2e84e66a6ca32c2dce294e36e428c6c859adf85
Kevin Ferreira <43164143+onthis@users.noreply.github.com>
2018-10-11 19:12:14 -0400

    Add files via upload

---
+++ Mr Sun Group Project/sketch.js
@@ -0,0 +1,93 @@
+var xPos;
+var yPos;
+var r;
+var g;
+var b;
+
+function setup() {
+
+	createCanvas(600,600);
+	rectMode(CENTER);
+
+}
+
+function draw() {
+
+	background(135,206,250);
+
+	myShape(mouseX,mouseY);
+
+	noStroke();
+
+}
+
+function myShape(xPos, yPos){
+
+	// FADING ARRAY OF GREEN SPOTS
+
+	for(var j = 0; j < 10; j++){
+
+		var yOffset = (55*j) + 50;
+
+		var green = map(j, 0, 9, 255, 230);
+
+		for(var i = 0; i < 10; i++){
+
+			var xOffset = (55*i) + 50;
+
+			var red = map(i, 0, 9, 25, 0);
+
+			fill(red,green,0);
+
+			ellipse(xOffset, yOffset, 30, 30);
+		}
+	}
+
+	// ROTATING ELLIPSES
+
+	push();
+
+	translate(300,300);
+
+  	for (var i = 0; i < 20; i++) {
+
+  		fill(r,g,b);
+
+    	ellipse(mouseX-300, mouseY-300, 20, 80);
+
+    	rotate(PI/10);
+
+    }
+
+    pop();
+
+    // SUN BODY
+
+	fill(255,255,0);
+
+	ellipse(300,300,100,100);
+
+	// WHITE PARTS OF SUN
+
+	fill(255);
+
+	rect(285,285,20,40);
+	rect(315,285,20,40);
+
+	// BLACK PARTS OF SUN
+
+	fill(0);
+
+	rect(285,285,10,30);
+	rect(315,285,10,30);
+	ellipse(300,325,40,10);
+
+}
+
+function mouseMoved(){
+
+	r = random(255);
+	g = random(255);
+	b = 0;
+
+}
